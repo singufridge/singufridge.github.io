3ceed7a6afaf8d12f114fb825548e5891f4c5e6a
singufridge <cadence.delia382@gmail.com>
2026-05-19 14:21:16 -0400

    added text change on click

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.querySelector("p").innerHTML = "this reading was cool";
+// document.querySelector("p").innerHTML = "this reading was cool";
+
+document.getElementById("header-2").addEventListener("click", myFirstFunction);
+
+function myFirstFunction() {    
+    document.getElementById("header-2").innerText = "this is a test";
+}
